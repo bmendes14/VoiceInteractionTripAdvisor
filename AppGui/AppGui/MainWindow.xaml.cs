900bb633f1b8246f0dfc7eeb254fe3d53392af02
Bruno Mendes <bmendes13@ua.pt>
2019-10-28 12:00:03 +0000

    Basic Voice search for restaurants with some filters like FoodType and Establishment Type

---
+++ AppGui/AppGui/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Linq;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Shapes;
+using System.Xml.Linq;
+using mmisharp;
+using Newtonsoft.Json;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace AppGui
+{
+
+    public partial class MainWindow : Window
+    {
+        TripAdviserService service;
+        private MmiCommunication mmiC;
+        public MainWindow()
+        {
+
+            InitializeComponent();
+            service = new TripAdviserService();
+
+            mmiC = new MmiCommunication("localhost",8000, "User1", "GUI");
+            mmiC.Message += MmiC_Message;
+            mmiC.Start();
+
+        }
+
+        private void MmiC_Message(object sender, MmiEventArgs e)
+        {
+            Console.WriteLine(e.Message);
+            var doc = XDocument.Parse(e.Message);
+            var com = doc.Descendants("command").FirstOrDefault().Value;
+            
+            dynamic json = JsonConvert.DeserializeObject(com);
+
+            
+
+            switch (json.recognized[0].ToString())
+            {
+                case "cidades":
+                    service.ShowPlace(json.recognized[1].ToString());
+                    break;
+
+                case "Comidas":
+                    service.ShowFood(json.recognized[1].ToString());
+                    break;
+
+                case "Tipodeestabelecimento":
+                    service.ShowEstablishment(json.recognized[1].ToString());
+                    break;
+
+            }
+
+
+        }
+        }
+}
---
+++ AppGui/AppGui/TripAdviserService.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Linq;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Shapes;
+using System.Xml.Linq;
+using mmisharp;
+using Newtonsoft.Json;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+
+namespace AppGui
+{
+    class TripAdviserService
+    {
+        private String startUrl = "https://www.tripadvisor.pt/";
+        private IWebDriver driver;
+
+
+        public TripAdviserService()
+        {
+            driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://www.tripadvisor.pt/");
+            driver.Manage().Window.Maximize();
+        }
+
+        public void baseURL()
+        {
+            OpenUrl(startUrl);
+        }
+
+        public void OpenUrl(String url)
+        {
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl(url);
+        }
+
+
+        public void ShowPlace(String place)
+        {
+            try
+            {
+                // nAinda não clica no sitio certo !! 
+                driver.FindElement(By.XPath("//span[@class='ui_icon restaurants brand-quick-links-QuickLinkTileItem__icon--2iguo']")).Click();
+                driver.FindElement(By.XPath("//input[@class='input-text-input-ManagedTextInput__managedInput--2RESp']")).SendKeys(place);
+                driver.FindElement(By.XPath("//span[@class='common-typeahead-results-BasicResult__resultTitle--1TQbu' and text()='" + place+"']")).Click();  
+            }
+            // when a method call is invalid for the object's current state.
+            catch (InvalidOperationException e)
+            {
+                return;
+            }// indicate that the element being requested does not exist.
+            catch (NoSuchElementException e)
+            {
+                return;
+            }
+        }
+
+        public void ShowFood(String food)
+        {
+            try
+            {
+                driver.FindElement(By.XPath("//span[@class='name' and text()='"+food+"']")).Click();
+
+              
+            }// when a method call is invalid for the object's current state.
+            catch (InvalidOperationException e)
+            {
+                return;
+            }// indicate that the element being requested does not exist.
+            catch (NoSuchElementException e)
+            {
+                return;
+            }
+        }
+
+        public void ShowEstablishment(String establishment)
+        {
+            try
+            {
+                driver.FindElement(By.XPath("//a[@onclick='event.preventDefault();' and text()='" + establishment + "']")).Click();
+
+
+            }// when a method call is invalid for the object's current state.
+            catch (InvalidOperationException e)
+            {
+                return;
+            }// indicate that the element being requested does not exist.
+            catch (NoSuchElementException e)
+            {
+                return;
+            }
+        }
+
+    }
+}
