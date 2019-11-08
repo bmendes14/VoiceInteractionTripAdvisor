f07673b58c44ef25ac0156e93534b4dc9da15e17
Fábio Nunes <fabioalexandrenunes@ua.pt>
2019-11-08 09:38:40 +0000

    Added confidence verification

--- AppGui/AppGui/MainWindow.xaml.cs
+++ AppGui/AppGui/MainWindow.xaml.cs
@@ -37,23 +37,21 @@ namespace AppGui
             
             dynamic json = JsonConvert.DeserializeObject(com);
 
-            
+         
 
             switch (json.recognized[0].ToString())
             {
                 case "cidades":
-                    service.ShowPlace(json.recognized[1].ToString());
+                    service.ShowPlace(json.recognized[1].ToString(),json.recognized[2].ToString());
                     break;
-
                 case "Comidas":
-                    service.ShowFood(json.recognized[1].ToString());
+                    service.ShowFood(json.recognized[1].ToString(), json.recognized[2].ToString());
                     break;
-
                 case "Tipodeestabelecimento":
-                    service.ShowEstablishment(json.recognized[1].ToString());
+                    service.ShowEstablishment(json.recognized[1].ToString(), json.recognized[2].ToString());
                     break;
                 case "Limpar":
-                    service.Clear();
+                    service.Clear(json.recognized[2].ToString());
                     break;
 
             }
--- AppGui/AppGui/TripAdviserService.cs
+++ AppGui/AppGui/TripAdviserService.cs
@@ -46,39 +46,88 @@ namespace AppGui
         }
 
 
-        public void ShowPlace(String place)
+        public void ShowPlace(String place, String confidence)
         {
             try
             {
-                
-               //verificação se botão de procurar cidades já se encontra aberto
-                if(FindElementIfExists(driver,By.XPath("//div[@class='_2EFRp_bb']"))!=null)
+                /** 
+                 * Se a confiança for maior que 80 -> No feedback
+                 *  >60e  menor ou igual que 80 -> Avisar a açºao
+                 *  menor que 60 pedir para repetir
+                 * **/
+                if (Convert.ToDouble(confidence) <= 80 && Convert.ToDouble(confidence) >= 60)
                 {
-                    driver.FindElement(By.XPath("//div[@class='_2EFRp_bb']")).Click();
-                    System.Threading.Thread.Sleep(1000);
-                    driver.FindElement(By.XPath("//span[@class='ui_icon restaurants brand-quick-links-QuickLinkTileItem__icon--2iguo']")).Click();
-                    driver.FindElement(By.XPath("//input[@class='input-text-input-ManagedTextInput__managedInput--2RESp']")).SendKeys(place);
-                    System.Threading.Thread.Sleep(1000);
-                    driver.FindElement(By.XPath("//span[@class='common-typeahead-results-BasicResult__resultTitle--1TQbu' and contains(translate(., 'ABCDEFGHIJKLMNOPQRSTUVWXYZ', 'abcdefghijklmnopqrstuvwxyz'),'" + place.ToLower() + "')]")).Click();
+                    String r = "A procurar restaurantes na cidade " + place;
+                    //verificação se botão de procurar cidades já se encontra aberto
+                    if (FindElementIfExists(driver, By.XPath("//div[@class='_2EFRp_bb']")) != null)
+                    {
+
+                        t.Speak(r);
+                        driver.FindElement(By.XPath("//div[@class='_2EFRp_bb']")).Click();
+                        System.Threading.Thread.Sleep(1000);
+                        driver.FindElement(By.XPath("//span[@class='ui_icon restaurants brand-quick-links-QuickLinkTileItem__icon--2iguo']")).Click();
+                        driver.FindElement(By.XPath("//input[@class='input-text-input-ManagedTextInput__managedInput--2RESp']")).SendKeys(place);
+                        System.Threading.Thread.Sleep(1000);
+                        driver.FindElement(By.XPath("//span[@class='common-typeahead-results-BasicResult__resultTitle--1TQbu' and contains(translate(., 'ABCDEFGHIJKLMNOPQRSTUVWXYZ', 'abcdefghijklmnopqrstuvwxyz'),'" + place.ToLower() + "')]")).Click();
+                    }
+                    //verificação se cidade já foi procurada e queremos procurar outra cidade
+                    else if (FindElementIfExists(driver, By.XPath("//span[@class='ui_icon caret-down brand-global-nav-geopill-GeoPill__icon--3Uykj']")) != null)
+                    {
+                        t.Speak(r);
+                        driver.FindElement(By.XPath("//span[@class='ui_icon caret-down brand-global-nav-geopill-GeoPill__icon--3Uykj']")).Click();
+                        System.Threading.Thread.Sleep(1000);
+                        driver.FindElement(By.XPath("//input[@class='input-text-input-ManagedTextInput__managedInput--2RESp']")).SendKeys(place);
+                        System.Threading.Thread.Sleep(1000);
+                        driver.FindElement(By.XPath("//span[@class='common-typeahead-results-BasicResult__resultTitle--1TQbu' and contains(translate(., 'ABCDEFGHIJKLMNOPQRSTUVWXYZ', 'abcdefghijklmnopqrstuvwxyz'),'" + place.ToLower() + "')]")).Click();
+                    }
+                    //caso seja o caso default, estamos na página principal sem ter procurado nenhuma cidade
+                    else
+                    {
+                        t.Speak(r);
+                        driver.FindElement(By.XPath("//span[@class='ui_icon restaurants brand-quick-links-QuickLinkTileItem__icon--2iguo']")).Click();
+                        driver.FindElement(By.XPath("//input[@class='input-text-input-ManagedTextInput__managedInput--2RESp']")).SendKeys(place);
+                        System.Threading.Thread.Sleep(1000);
+                        driver.FindElement(By.XPath("//span[@class='common-typeahead-results-BasicResult__resultTitle--1TQbu' and contains(translate(., 'ABCDEFGHIJKLMNOPQRSTUVWXYZ', 'abcdefghijklmnopqrstuvwxyz'),'" + place.ToLower() + "')]")).Click();
+
+                    }
                 }
-                //verificação se cidade já foi procurada e queremos procurar outra cidade
-                   else if(FindElementIfExists(driver,By.XPath("//span[@class='ui_icon caret-down brand-global-nav-geopill-GeoPill__icon--3Uykj']"))!=null)
+                else if(Convert.ToDouble(confidence) < 60)
                 {
-                    driver.FindElement(By.XPath("//span[@class='ui_icon caret-down brand-global-nav-geopill-GeoPill__icon--3Uykj']")).Click();
+                    t.Speak("Não consegui entender, será possível repetir?");
                     System.Threading.Thread.Sleep(1000);
-                    driver.FindElement(By.XPath("//input[@class='input-text-input-ManagedTextInput__managedInput--2RESp']")).SendKeys(place);
-                    System.Threading.Thread.Sleep(1000);
-                    driver.FindElement(By.XPath("//span[@class='common-typeahead-results-BasicResult__resultTitle--1TQbu' and contains(translate(., 'ABCDEFGHIJKLMNOPQRSTUVWXYZ', 'abcdefghijklmnopqrstuvwxyz'),'" + place.ToLower() + "')]")).Click();
                 }
-                //caso seja o caso default, estamos na página principal sem ter procurado nenhuma cidade
                 else
                 {
-                    driver.FindElement(By.XPath("//span[@class='ui_icon restaurants brand-quick-links-QuickLinkTileItem__icon--2iguo']")).Click();
-                    driver.FindElement(By.XPath("//input[@class='input-text-input-ManagedTextInput__managedInput--2RESp']")).SendKeys(place);
-                    System.Threading.Thread.Sleep(1000);
-                    driver.FindElement(By.XPath("//span[@class='common-typeahead-results-BasicResult__resultTitle--1TQbu' and contains(translate(., 'ABCDEFGHIJKLMNOPQRSTUVWXYZ', 'abcdefghijklmnopqrstuvwxyz'),'" + place.ToLower() + "')]")).Click();
+                    //verificação se botão de procurar cidades já se encontra aberto
+                    if (FindElementIfExists(driver, By.XPath("//div[@class='_2EFRp_bb']")) != null)
+                    {
+                        driver.FindElement(By.XPath("//div[@class='_2EFRp_bb']")).Click();
+                        System.Threading.Thread.Sleep(1000);
+                        driver.FindElement(By.XPath("//span[@class='ui_icon restaurants brand-quick-links-QuickLinkTileItem__icon--2iguo']")).Click();
+                        driver.FindElement(By.XPath("//input[@class='input-text-input-ManagedTextInput__managedInput--2RESp']")).SendKeys(place);
+                        System.Threading.Thread.Sleep(1000);
+                        driver.FindElement(By.XPath("//span[@class='common-typeahead-results-BasicResult__resultTitle--1TQbu' and contains(translate(., 'ABCDEFGHIJKLMNOPQRSTUVWXYZ', 'abcdefghijklmnopqrstuvwxyz'),'" + place.ToLower() + "')]")).Click();
+                    }
+                    //verificação se cidade já foi procurada e queremos procurar outra cidade
+                    else if (FindElementIfExists(driver, By.XPath("//span[@class='ui_icon caret-down brand-global-nav-geopill-GeoPill__icon--3Uykj']")) != null)
+                    {
+                        driver.FindElement(By.XPath("//span[@class='ui_icon caret-down brand-global-nav-geopill-GeoPill__icon--3Uykj']")).Click();
+                        System.Threading.Thread.Sleep(1000);
+                        driver.FindElement(By.XPath("//input[@class='input-text-input-ManagedTextInput__managedInput--2RESp']")).SendKeys(place);
+                        System.Threading.Thread.Sleep(1000);
+                        driver.FindElement(By.XPath("//span[@class='common-typeahead-results-BasicResult__resultTitle--1TQbu' and contains(translate(., 'ABCDEFGHIJKLMNOPQRSTUVWXYZ', 'abcdefghijklmnopqrstuvwxyz'),'" + place.ToLower() + "')]")).Click();
+                    }
+                    //caso seja o caso default, estamos na página principal sem ter procurado nenhuma cidade
+                    else
+                    {
+                        driver.FindElement(By.XPath("//span[@class='ui_icon restaurants brand-quick-links-QuickLinkTileItem__icon--2iguo']")).Click();
+                        driver.FindElement(By.XPath("//input[@class='input-text-input-ManagedTextInput__managedInput--2RESp']")).SendKeys(place);
+                        System.Threading.Thread.Sleep(1000);
+                        driver.FindElement(By.XPath("//span[@class='common-typeahead-results-BasicResult__resultTitle--1TQbu' and contains(translate(., 'ABCDEFGHIJKLMNOPQRSTUVWXYZ', 'abcdefghijklmnopqrstuvwxyz'),'" + place.ToLower() + "')]")).Click();
 
+                    }
                 }
+                   
             }
             // when a method call is invalid for the object's current state.
             catch (InvalidOperationException e)
@@ -91,7 +140,7 @@ namespace AppGui
             }
         }
 
-        public void Clear()
+        public void Clear(String confidence)
         {
             try
             {
@@ -101,9 +150,23 @@ namespace AppGui
                 ////span[@class='clear' and contains(Limpar tudo )]
                 if (FindElementIfExists(driver, By.XPath("//span[@class='clear']")) != null)
                 {                   
-                    t.Speak("A Limpar");
-                    System.Threading.Thread.Sleep(1000);
-                    driver.FindElement(By.XPath("//span[@class='clear']")).Click();
+                    if(Convert.ToDouble(confidence) <= 80 && Convert.ToDouble(confidence) >=60)
+                    {
+                        t.Speak("A Limpar filtros");
+                        System.Threading.Thread.Sleep(1000);
+                        driver.FindElement(By.XPath("//span[@class='clear']")).Click();
+                    }
+                    else if (Convert.ToDouble(confidence) < 60)
+                    {
+                        t.Speak("Não consegui entender, será possível repetir?");
+                        System.Threading.Thread.Sleep(1000);
+                    }
+                    else
+                    {
+                        System.Threading.Thread.Sleep(1000);
+                        driver.FindElement(By.XPath("//span[@class='clear']")).Click();
+                    }
+                    
                 }
                 else
                 {
@@ -126,7 +189,7 @@ namespace AppGui
         }
 
 
-        public void ShowFood(String food)
+        public void ShowFood(String food, String confidence)
         {
             try
             {
@@ -144,7 +207,7 @@ namespace AppGui
             }
         }
 
-        public void ShowEstablishment(String establishment)
+        public void ShowEstablishment(String establishment, String confidence)
         {
             try
             {
